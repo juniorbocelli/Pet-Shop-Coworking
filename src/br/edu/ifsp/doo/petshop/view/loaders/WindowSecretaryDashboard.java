eea290ad5a95e948d7cf72bd459327a8959d5dfe
Júnior Bocelli <juniorbocelli@gmail.com>
2020-08-05 11:49:35 -0300

    FXML of the Veterinary Dashboard.

--- src/br/edu/ifsp/doo/petshop/view/loaders/WindowSecretaryDashboard.java
+++ src/br/edu/ifsp/doo/petshop/view/loaders/WindowSecretaryDashboard.java
@@ -1,4 +1,30 @@
 package br.edu.ifsp.doo.petshop.view.loaders;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class WindowSecretaryDashboard {
+    public void showClientsList(ActionEvent actionEvent) {
+    }
+
+    public void showVetsList(ActionEvent actionEvent) {
+    }
+
+    public void showProductsList(ActionEvent actionEvent) {
+    }
+
+    public void showAnimalsList(ActionEvent actionEvent) {
+    }
+
+    public void showUserData(MouseEvent mouseEvent) {
+    }
+
+    public void goToDate(ActionEvent actionEvent) {
+    }
+
+    public void goToToday(ActionEvent actionEvent) {
+    }
+
+    public void goToVeterinary(ActionEvent actionEvent) {
+    }
 }
