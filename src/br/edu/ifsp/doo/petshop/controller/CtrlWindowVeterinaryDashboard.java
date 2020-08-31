e451c3e491be9e475878d406f40d3064b2ab2db6
Júnior Bocelli <juniorbocelli@gmail.com>
2020-08-31 19:36:48 -0300

    VeterinaryDashboard

--- src/br/edu/ifsp/doo/petshop/controller/CtrlWindowVeterinaryDashboard.java
+++ src/br/edu/ifsp/doo/petshop/controller/CtrlWindowVeterinaryDashboard.java
@@ -1,14 +1,17 @@
 package br.edu.ifsp.doo.petshop.controller;
 
 import br.edu.ifsp.doo.petshop.main.Main;
+import br.edu.ifsp.doo.petshop.model.entities.Consultation;
 import br.edu.ifsp.doo.petshop.model.entities.Veterinary;
-import br.edu.ifsp.doo.petshop.view.loaders.WindowAnimalManager;
-import br.edu.ifsp.doo.petshop.view.loaders.WindowBilling;
-import br.edu.ifsp.doo.petshop.view.loaders.WindowVeterinary;
-import br.edu.ifsp.doo.petshop.view.loaders.WindowVeterinaryRecords;
+import br.edu.ifsp.doo.petshop.model.usecases.UCManageVeterinary;
+import br.edu.ifsp.doo.petshop.persistence.dao.DAOVeterinary;
+import br.edu.ifsp.doo.petshop.view.loaders.*;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -17,6 +20,7 @@ import javafx.util.Duration;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 public class CtrlWindowVeterinaryDashboard {
     @FXML DatePicker txtDate;
@@ -31,10 +35,25 @@ public class CtrlWindowVeterinaryDashboard {
     @FXML Label lblCurrentDate;
     @FXML Label lblUserName;
 
+    @FXML TableView<Consultation> tblSchedule;
+
+    @FXML TableColumn<Consultation, String> clnStartTime;
+    @FXML TableColumn<Consultation, String> clnEndTime;
+    @FXML TableColumn<Consultation, String> clnClient;
+    @FXML TableColumn<Consultation, String> clnAnimal;
+    @FXML TableColumn<Consultation, String> clnStatus;
+
     private Veterinary veterinary;
+
+    private ObservableList<Consultation> tableData;
+    private List<Consultation> allConsultations;
+
+    private UCManageVeterinary ucManageVeterinary;
 
     public CtrlWindowVeterinaryDashboard() {
         veterinary = (Veterinary) Main.getInstance().getLoggedUser();
+
+        ucManageVeterinary = new UCManageVeterinary(new DAOVeterinary());
     }
 
     @FXML
@@ -70,10 +89,13 @@ public class CtrlWindowVeterinaryDashboard {
     }
 
     public void changeDate(ActionEvent actionEvent) {
+        loadDataAndShow();
     }
 
     public void goToToday(ActionEvent actionEvent) {
         txtDate.setValue(LocalDate.now());
+
+        loadDataAndShow();
     }
 
     public void manageVeterinaryConsultations(ActionEvent actionEvent) {
@@ -94,5 +116,45 @@ public class CtrlWindowVeterinaryDashboard {
     public void manageAnimals(ActionEvent actionEvent) {
         WindowAnimalManager windowAnimalManager = new WindowAnimalManager();
         windowAnimalManager.startModal();
+    }
+
+    private LocalDate getDateFromView() {
+        return txtDate.getValue();
+    }
+
+    public void manageConsultations(MouseEvent mouseEvent) {
+        Consultation selectedConsultation = tblSchedule.getSelectionModel().getSelectedItem();
+        if (mouseEvent.getClickCount() == 2) {
+            if (selectedConsultation != null) {
+                WindowConsultation windowConsultation = new WindowConsultation();
+                windowConsultation.startModal(selectedConsultation);
+            } else {
+                WindowConsultation windowConsultation = new WindowConsultation();
+                windowConsultation.startModal();
+            }
+
+            loadDataAndShow();
+        }
+    }
+
+    public void bindTableViewToItemsList() {
+        tableData = FXCollections.observableArrayList();
+        tblSchedule.setItems(tableData);
+    }
+
+    public void bindColumnsToValueSources() {
+        clnStartTime.setCellValueFactory((param) -> new SimpleStringProperty(param.getValue().getTimeLapse().getStartTime().toString().substring(11, 16)));
+        clnEndTime.setCellValueFactory((param) -> new SimpleStringProperty(param.getValue().getTimeLapse().getEndTime().toString().substring(11, 16)));
+        clnAnimal.setCellValueFactory((param) -> new SimpleStringProperty(param.getValue().getAnimal().getName()));
+        clnStatus.setCellValueFactory((param) -> new SimpleStringProperty(param.getValue().isPaid() ? "Pago":"Não Pago"));
+    }
+
+    private void loadDataAndShow() {
+        loadTableDataFromDatabase();
+    }
+
+    private void loadTableDataFromDatabase() {
+        allConsultations = ucManageVeterinary.selectConsultationsList(veterinary, getDateFromView());
+        tableData.setAll(allConsultations);
     }
 }
